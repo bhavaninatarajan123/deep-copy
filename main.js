e5ffab9448306cdec0eff9e1eae151e7f41e483d
priyankacute <bhavanimurugesh2000@gmail.com>
2023-10-27 19:53:43 +0530

    addnew

---
+++ main.js
@@ -0,0 +1,12 @@
+
+var a=10;
+    var b=a;
+let num=(a,b)=>{
+    
+    var b=34;
+    document.write("the value of a is :"+a+"<br>");
+    document.write("the value of a is :"+b);
+
+}
+num(a,b);
+
